1e628ecbb50d0a76911dd77bc7afe766edce4249
zeuss254 <ckarimi676@gmail.com>
2023-02-18 13:16:12 +0000

    update

--- 0x00-hello_world/4_puts.c
+++ 0x00-hello_world/4_puts.c
@@ -3,13 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-      return (0);
-
-
+return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,22 @@
 #include<stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+  */
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
 
-int main(void){
- char a;
- int b;
- long int c;
- long long int d;
- float e;
-
-
-
-
-
-    	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of a int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 	printf("Size of a float: %zu byte(s)\n", sizeof(e));
-
-	return 0;
-	
-
+return (0);
 }
