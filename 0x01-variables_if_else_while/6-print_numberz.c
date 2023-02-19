710f949ed7d710c09835e658e9eb3577dcd3450f
zeuss254 <ckarimi676@gmail.com>
2023-02-19 18:03:55 +0000

    update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+  * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = '0'; i < '10'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
