8d14fb906a249108552ee5488a69e713552f1ecd
zeuss254 <ckarimi676@gmail.com>
2023-02-19 13:51:34 +0000

    update

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char get;
+
+	for (get = 'a'; get <= 'z'; get++)
+	{
+		putchar(get);
+		putchar('\n');
+	}
+	for (get = 'A'; get <= 'Z'; get++)
+	{
+		putchar(get);
+		putchar('\n');
+	}
+	return (0);
+}
