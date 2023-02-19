a8b43c2d44c356c9b927d500c74c980aef8dbe0c
zeuss254 <ckarimi676@gmail.com>
2023-02-19 13:19:17 +0000

    update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.>
+#include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char get;
+
+	for (get = 'a'; get <= 'z'; get++)
+	{
+	Putchar(get);
+	Putchar('\n');
+	}
+	return (0);
+}
