a7cd09ef678187fea9d94accbb047a688f23972e
zeuss254 <ckarimi676@gmail.com>
2023-02-19 17:43:44 +0000

    update

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char get, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (get = 'a'; get <= 'z'; get++)
+	{
+		if (low != e && low != q)
+			putchar(get);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Successs)
+ */
+int main(void)
+{
+
+	int i;
+
+	for (i = 0; i <= 10; i++)
+		printf("%d\n" i)
+
+	return (0);
+}
