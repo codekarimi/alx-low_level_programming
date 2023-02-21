151ba00fe3c86b2088bb722531c9bde559f5cb17
zeuss254 <ckarimi676@gmail.com>
2023-02-21 19:54:05 +0000

    update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include<main.h>
+
+/**
+ * _islower -Entry point
+ *@c: An input character
+ * Return: 1 if lowercase 0 if otherwise
+ */
+int _islower(int c)
+{
+
+	char k;
+	int l = 0;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		if (k == 'c')
+			l = 1;
+	}
+
+	return (lower)
+}
