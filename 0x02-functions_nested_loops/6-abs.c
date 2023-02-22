78b82b613733fd8619b7321571f0922e21c49c29
zeuss254 <ckarimi676@gmail.com>
2023-02-22 07:19:43 +0000

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * _abs - Entry point
+ * @r:number input
+ *Return: Absolute value of number r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
