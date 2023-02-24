02578fe132add1c960f2dfed1d7193f7d447148d
zeuss254 <ckarimi676@gmail.com>
2023-02-24 12:00:07 +0000

    update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ *Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i = '0';
+
+	for (; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
