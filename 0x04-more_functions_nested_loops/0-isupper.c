d48c0b42e727d768643141298fed05289871801a
zeuss254 <ckarimi676@gmail.com>
2023-02-24 10:55:02 +0000

    update

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include"main.h"
+
+/**
+ * _isupper - Entry point
+ * @c:input integer
+ *Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	char upper;
+	int islower;
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+	{
+		if (c == uppercase)
+		{
+			islower = 1;
+		}
+		else
+		{
+			islower = 0;
+		}
+	}
+	return (islower);
+}
