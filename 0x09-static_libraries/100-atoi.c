247cd7326e59600524b62eeb45f109ccd3e52102
zeuss254 <ckarimi676@gmail.com>
2023-03-12 15:36:15 +0000

    update

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _atoi - a function that converts string to integer
+ * @s: An input string
+ * Return: integer from conversion
+ */
+int _atoi(char *s)
+{
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: an input string
+ * @accept: an input character with to locate into string s
+ * Return: returns pointer to c position
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+}
